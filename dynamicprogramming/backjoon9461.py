644d190af67f7b032dccf272f7e3e4ece9c6aea8
mck0324 <dch3030@naver.com>
2024-09-04 17:31:59 +0900

    DP(Dynamic Programming)

---
+++ dynamicprogramming/backjoon9461.py
@@ -0,0 +1,12 @@
+def problem_11726(num):
+    dp = [0]*101
+    dp[1] = 1
+    dp[2] = 1
+    dp[3] = 1
+    for i in range(4,101):
+        dp[i] = dp[i-2] + dp[i-3]
+    return dp[num]        
+    
+print(problem_11726(2)) 
+print(problem_11726(6)) 
+print(problem_11726(12)) 
